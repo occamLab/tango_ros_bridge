f158e4ab442c5026ec27e5f85debefe871ffdfe7
Paul Ruvolo <paullundyruvolo@gmail.com>
2015-03-13 10:44:31 -0400

    added some new stuff in make depth image

--- tango_streamer/scripts/make_depth_image.py
+++ tango_streamer/scripts/make_depth_image.py
@@ -12,6 +12,9 @@ from threading import Lock
 class DepthImageCreator(object):
 	def __init__(self):
 		self.depth_image_lock = Lock()
+		self.image_list_lock = Lock()
+		self.image_list = []
+		self.image_list_max_size = 100
 		self.downsample_factor = 2
 		self.tf = TransformListener()
 		rospy.Subscriber("/camera_info",
@@ -33,9 +36,12 @@ class DepthImageCreator(object):
 		self.image = None
 		self.last_image_timestamp = None
 		self.click_timestamp = None
+		self.depth_timestamp = None
 		cv2.namedWindow("depth_feed")
 		cv2.namedWindow("image_feed")
+		cv2.namedWindow("combined_feed")
 		cv2.setMouseCallback('image_feed',self.handle_click)
+		cv2.setMouseCallback('combined_feed',self.handle_combined_click)
 
 	def handle_click(self,event,x,y,flags,param):
 		if event == cv2.EVENT_LBUTTONDOWN:
@@ -43,9 +49,17 @@ class DepthImageCreator(object):
 			self.click_coords = (x*self.downsample_factor,y*self.downsample_factor)
 
 	def process_image(self,msg):
+		self.image_list_lock.acquire()
+		if self.last_image_timestamp and msg.header.stamp < self.last_image_timestamp:
+			# handle backward jumps in time (shoudl only happen when playing back a bag)
+			self.tf.clear()
 		np_arr = np.fromstring(msg.data, np.uint8)
 		self.last_image_timestamp = msg.header.stamp
 		self.image = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
+		if len(self.image_list) == self.image_list_max_size:
+			self.image_list.pop(0)
+		self.image_list.append((msg.header.stamp,self.image))
+		self.image_list_lock.release()
 
 	def process_camera_info(self, msg):
 		self.camera_info = msg
@@ -54,38 +68,77 @@ class DepthImageCreator(object):
 		# TODO: this is necessary due to a mistake in intrinsics_server.py
 		self.D = np.array([msg.D[0],msg.D[1],0,0,msg.D[2]])
 
+	def get_nearest_image_temporally(self,timestamp):
+		self.image_list_lock.acquire()
+		diff_list = []
+		for im_stamp,image in self.image_list:
+			diff_list.append((abs((im_stamp-timestamp).to_sec()),image))
+		closest_temporally = min(diff_list,key=lambda t: t[0])
+		self.image_list_lock.release()
+		print closest_temporally[0]
+		return closest_temporally[1]
+
+	def handle_combined_click(self,event,x,y,flags,param):
+		if event == cv2.EVENT_LBUTTONDOWN:
+			try:
+				self.depth_image_lock.acquire()
+				click_coords = (x*self.downsample_factor,y*self.downsample_factor)
+				distances = []
+				for i in range(self.projected_points.shape[0]):
+					dist = (self.projected_points[i,0,0] - click_coords[0])**2 + (self.projected_points[i,0,1] - click_coords[1])**2
+					distances.append(dist)
+				three_d_coord = self.points_3d[:,np.argmin(distances)]
+
+				# again, we have to reshuffle the coordinates due to differences in ROS Tango coordinate systems
+				point_msg = PointStamped(header=Header(stamp=self.depth_image_timestamp,
+													   frame_id="depth_camera"),
+										 point=Point(y=three_d_coord[0],
+												 	 z=three_d_coord[1],
+												 	 x=three_d_coord[2]))
+				self.tf.waitForTransform("depth_camera",
+										 "odom",
+										 self.depth_image_timestamp,
+										 rospy.Duration(1.0))
+				transformed_coord = self.tf.transformPoint('odom', point_msg)
+				self.clicked_point_pub.publish(transformed_coord)
+				self.depth_image_lock.release()
+			except Exception as ex:
+				print "Encountered an errror! ", ex
+				self.depth_image_lock.release()
+
+
 	def process_point_cloud(self, msg):
 		self.depth_image_lock.acquire()
 		try:
 			if self.P == None:
 				self.depth_image_lock.release()
 				return
-
+			self.depth_image_timestamp = msg.header.stamp
 			self.depth_image = np.zeros((self.camera_info.height, self.camera_info.width, 3),dtype=np.uint8)
-			points_3d = np.zeros((3,len(msg.points))).astype(np.float32)
+			self.points_3d = np.zeros((3,len(msg.points))).astype(np.float32)
 			depths = []
 			for i,p in enumerate(msg.points):
 				# this is weird due to mismatches between Tango coordinate system and ROS
-				points_3d[:,i] = np.array([p.y, p.z, p.x])
+				self.points_3d[:,i] = np.array([p.y, p.z, p.x])
 				depths.append(p.x)
-			projected_points, dc = cv2.projectPoints(points_3d.T,
-												 	 (0,0,0),
-												 	 (0,0,0),
-												 	 self.K,
-												 	 self.D)
+			self.projected_points, dc = cv2.projectPoints(self.points_3d.T,
+												 		  (0,0,0),
+												 		  (0,0,0),
+												 		  self.K,
+														  self.D)
 
 			if self.click_timestamp != None and msg.header.stamp > self.click_timestamp:
 				distances = []
-				for i in range(projected_points.shape[0]):
-					dist = (projected_points[i,0,0] - self.click_coords[0])**2 + (projected_points[i,0,1] - self.click_coords[1])**2
+				for i in range(self.projected_points.shape[0]):
+					dist = (self.projected_points[i,0,0] - self.click_coords[0])**2 + (self.projected_points[i,0,1] - self.click_coords[1])**2
 					distances.append(dist)
-				three_d_coord = points_3d[:,np.argmin(distances)]
+				three_d_coord = self.points_3d[:,np.argmin(distances)]
 				# again, we have to reshuffle the coordinates due to differences in ROS Tango coordinate systems
 				point_msg = PointStamped(header=Header(stamp=msg.header.stamp,
 													   frame_id="depth_camera"),
 										 point=Point(y=three_d_coord[0],
-										 			 z=three_d_coord[1],
-										 			 x=three_d_coord[2]))
+													 z=three_d_coord[1],
+													 x=three_d_coord[2]))
 				transformed_coord = self.tf.transformPoint('odom', point_msg)
 				self.clicked_point_pub.publish(transformed_coord)
 				self.click_timestamp = None
@@ -94,13 +147,12 @@ class DepthImageCreator(object):
 			depths_equalized = np.zeros((len(depths),1))
 			for idx,(i,depth) in enumerate(sorted(enumerate(depths), key=lambda x: -x[1])):
 				depths_equalized[i] = idx/float(len(depths)-1)
-
-			print projected_points.shape
-			for i in range(projected_points.shape[0]):
-				if not(np.isnan(projected_points[i,0,0])) and not(np.isnan(projected_points[i,0,1])):
-					self.depth_image[int(projected_points[i,0,1]),int(projected_points[i,0,0]),:] = int(depths_equalized[i]*255.0)
+			for i in range(self.projected_points.shape[0]):
+				if not(np.isnan(self.projected_points[i,0,0])) and not(np.isnan(self.projected_points[i,0,1])):
+					self.depth_image[int(self.projected_points[i,0,1]),int(self.projected_points[i,0,0]),:] = int(depths_equalized[i]*255.0)
 			self.depth_image_lock.release()
-		except:
+		except Exception as ex:
+			print "Encountered an errror! ", ex
 			self.depth_image_lock.release()
 
 	def run(self):
@@ -118,7 +170,18 @@ class DepthImageCreator(object):
 
 			if self.image != None:
 				cv2.imshow("image_feed", cv2.resize(self.image,(self.image.shape[1]/self.downsample_factor,
-																self.image.shape[0]/self.downsample_factor)))
+															self.image.shape[0]/self.downsample_factor)))
+			if self.image != None and self.depth_image != None:
+				kernel = np.ones((3,3),'uint8')
+				self.depth_image_lock.acquire()
+				nearest_image = self.get_nearest_image_temporally(self.depth_image_timestamp)
+				ret, depth_threshed = cv2.threshold(self.depth_image,1,255,cv2.THRESH_BINARY)
+				combined_img = (cv2.dilate(depth_threshed,kernel)*0.2 + nearest_image*0.8).astype(dtype=np.uint8)
+				cv2.imshow("combined_feed", cv2.resize(combined_img,(self.image.shape[1]/self.downsample_factor,
+													   self.image.shape[0]/self.downsample_factor)))
+
+				self.depth_image_lock.release()
+
 			r.sleep()
 
 if __name__ == '__main__':
