a5503cfa4881837d22b3d1755344cb12dbcb5599
Dhasharath Shrivathsa <Dhash@outlook.com>
2016-06-02 14:48:00 -0400

    UDP version trials

--- tango_streamer/scripts/image_server.py
+++ tango_streamer/scripts/image_server.py
@@ -1,96 +1,96 @@
 #!/usr/bin/env python
 
-"""
-A simple echo server
-"""
-
 import socket
 import rospy
+import sys
+
 from std_msgs.msg import Float64
 from sensor_msgs.msg import CompressedImage, PointCloud
 from geometry_msgs.msg import PoseStamped, Point32
-import sys
-
-# need to split sensor streams onto multiple ports and send only when
-# new data is available
-# need to better compress point clouds
 
 tango_clock_offset = 0
 
 def handle_tango_clock(msg):
-	global tango_clock_offset
-	tango_clock_offset = msg.data
+    global tango_clock_offset
+    tango_clock_offset = msg.data
 
-rospy.init_node("image_server") 
 
-host = ''
+#rospy to interface with ROS setup
+rospy.init_node("image_server")
+
+host = '' 
 port = rospy.get_param('~port_number')
 camera_name = rospy.get_param('~camera_name')
 
 clock_sub = rospy.Subscriber('/tango_clock', Float64, handle_tango_clock)
 pub_camera = rospy.Publisher('/' + camera_name + '/image_raw/compressed', CompressedImage, queue_size=10)
 
-backlog = 5
-size = 10**6
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host,port))
-s.listen(backlog)
-all_data = ''
-begin_frame_marker = 'DEPTHFRAMESTARTINGRIGHTNOW\n' 
+#encoding details
+begin_frame_marker = 'DEPTHFRAMESTARTINGRIGHTNOW\n'
 end_frame_marker = 'DEPTHFRAMEENDINGRIGHTNOW\n'
-
-begin_timestamp_marker = 'DEPTHTIMESTAMPSTARTINGRIGHTNOW\n' 
+begin_timestamp_marker = 'DEPTHTIMESTAMPSTARTINGRIGHTNOW\n'
 end_timestamp_marker = 'DEPTHTIMESTAMPENDINGRIGHTNOW\n'
 
-timestamp = 0
+#socket details
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((host, port))
 
-frame_num = 0
+#data storage
+backlog = ""
+
+
+
+#Loop to sit on socket and grab data
 while True:
-	client, address = s.accept()
-	while True:
-		try:
-			data = client.recv(size)
-			if not data:
-				break
-			all_data += data
 
-			index = all_data.find(end_frame_marker)
-			try:
-				if index != -1:
-					start = all_data.find(begin_frame_marker)
-					jpg = all_data[start+len(begin_frame_marker):index]
+    #blocks until can grab udp packet
+    data = sock.recv(65535)
+    if not data:
+        break
 
-					index_ts = jpg.find(end_timestamp_marker)
-					try:
-						if index != -1:
-							start_ts = jpg.find(begin_timestamp_marker)
-							timestamp = jpg[start_ts+len(begin_timestamp_marker):index_ts]
-							jpg = jpg[index_ts+len(end_timestamp_marker):]
-					except:
-						# assume we had a bogus message
-						all_data = ""
+    #add it to the to-be-processed data
+    backlog += data
+    
+    #Mark that there might be pictures in `backlog`
+    pictures_remain = True
 
-					print len(jpg)
-					msg = CompressedImage()
-					msg.header.stamp = rospy.Time(tango_clock_offset + float(timestamp))
-					print "overall offset", msg.header.stamp - rospy.Time.now()
-					msg.header.frame_id = camera_name
-					msg.data = jpg
-					msg.format = 'jpeg'
-					pub_camera.publish(msg)
-					frame_num += 1
-					all_data = all_data[index+len(end_frame_marker):]
-					print frame_num
-			except:
-				# assume we had a bogus message
-				all_data = ""
+    #Loop to search backlog for pictures
+    while pictures_remain: 
+        jpeg_begin_loc = backlog.find(begin_frame_marker)
+        jpeg_end_loc = backlog.find(end_frame_marker)
+        
+        #It found a picture!
+        if jpeg_begin_loc != -1 and  jpeg_end_loc != -1:
+            jpg = backlog[jpeg_begin_loc+len(begin_frame_marker):jpeg_end_loc]
+            backlog = backlog[jpeg_eng_loc+len(end_frame_marker):]
+
+            ts_begin_loc = jpg.find(begin_timestamp_marker)
+            ts_end_loc = jpg.find(end_timestamp_marker)
+
+            if ts_begin_loc == -1 or ts_end_loc == -1:
+                #something's fishy, discard jpeg
+                print("JPEG discarded, malformed data")
+                break
+                
+            ts = jpg[ts_begin_loc+len(begin_timestamp_marker):ts_end_marker]
+            jpg = jpg[ts_end_loc+len(end_timestamp_marker):]
+
+            print "{} bytes of JPEG recvd".format(len(jpg))
+            msg = CompressedImage()
+            msg.header.stamp  = rospy.Time(tango_clock_offset + float(ts))
+            msg.header.frame_id = camera_name
+            msg.data = jpg
+            msg.format = 'jpeg'
+            pub_camera.publish(msg)
 
 
-		except socket.error, msg:
-			sys.stderr.write('ERROR: %s\n' % msg)
-			#probably got disconnected
-			all_data = ''
-			print "DISCONNECTED"
-			break
-	print "CLOSING!"
-	client.close()
+        #no end bytes in sight
+        elif jpeg_end_loc == -1:
+            pictures_remain = False
+
+        #no beginning bytes in sight
+        elif jpeg_begin_loc == -1:
+            backlog = ""
+            pictures_remain = False
+
+
