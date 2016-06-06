7f522853eba158e2af9a0c3a50b6b677825ac8df
Paul Ruvolo <paullundyruvolo@gmail.com>
2016-06-06 15:05:02 -0400

    added  wifi scanning

--- android_code/hello-tango-jni-example/app/src/main/java/com/projecttango/experiments/nativehellotango/HelloTangoActivity.java
+++ android_code/hello-tango-jni-example/app/src/main/java/com/projecttango/experiments/nativehellotango/HelloTangoActivity.java
@@ -25,10 +25,13 @@ import com.projecttango.experiments.nativehellotango.TangoInitializationHelper;
 import java.io.BufferedReader;
 import java.nio.ByteBuffer;
 
+import android.net.wifi.WifiManager;
+import android.net.wifi.ScanResult;
 import android.graphics.Bitmap;
-
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.content.Context;
 import com.google.tango.hellotangojni.R;
-
 import android.os.IBinder;
 import android.app.Activity;
 import android.widget.ImageView;
@@ -63,6 +66,7 @@ import android.content.ComponentName;
 import android.content.ServiceConnection;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Main activity controls Tango lifecycle.
@@ -73,6 +77,8 @@ public class HelloTangoActivity extends Activity {
     private SharedPreferences preferences;
     private SharedPreferences.Editor preferencesEditor;
 
+    private boolean scanWifi = false;
+
     private boolean connected = false;
     private String hostName = "";
     final int portNumberColorImages = 11111;
@@ -82,6 +88,9 @@ public class HelloTangoActivity extends Activity {
     final int portNumberFisheyeImages = 11115;
     final int portNumberIntrinsicsFisheye = 11116;
     final int portNumberPoseArea = 11117;
+    final int portNumberWIFIScan = 11118;
+
+    private DatagramPacket scanPacket = null;
 
     private DatagramSocket udpFisheyeImages;
     private DatagramSocket udpColorImages;
@@ -90,6 +99,7 @@ public class HelloTangoActivity extends Activity {
     private DatagramSocket udpAreaPose;
     private DatagramSocket udpIntrinsicsColor;
     private DatagramSocket udpIntrinsicsFisheye;
+    private DatagramSocket udpWIFIScan = null;
 
     private Thread imagesColorThread;
     private Thread imagesFisheyeThread;
@@ -98,11 +108,42 @@ public class HelloTangoActivity extends Activity {
     private Thread pointCloudThread;
     private Thread intrinsicsColorThread;
     private Thread intrinsicsFisheyeThread;
+    private Thread scanThread;
 
     protected Bitmap bmColor;
     protected Bitmap bmFisheye;
 
     private InetAddress remote;
+
+    /**
+     * Broadcast receiver to update
+     */
+    private BroadcastReceiver myRssiChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context arg0, Intent arg1) {
+            WifiManager wifiMan = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+            if (udpWIFIScan != null && remote != null && scanPacket == null) {
+                ByteArrayOutputStream udpPayload = new ByteArrayOutputStream();
+                List<ScanResult> detected_aps = wifiMan.getScanResults();
+                System.out.println("RSSI: about to create message");
+                try {
+                    udpPayload.write("RSSISTART\n".getBytes());
+                    for (int i = 0; i < detected_aps.size(); i++) {
+                        System.out.println("RSSI: serializing BSSID" + i);
+                        udpPayload.write(("BSSID " + detected_aps.get(i).BSSID + " " + detected_aps.get(i).level + "\n").getBytes());
+                    }
+                    udpPayload.write("RSSIEND\n".getBytes());
+                    byte[] payload = udpPayload.toByteArray();
+                    scanPacket = new DatagramPacket(payload, payload.length, remote, portNumberWIFIScan);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("RSSI: received scan");
+            }
+            wifiMan.startScan();
+        }
+    };
 
     // Project Tango Service connection.
     ServiceConnection mTangoServiceConnection = new ServiceConnection() {
@@ -127,6 +168,7 @@ public class HelloTangoActivity extends Activity {
                 udpAreaPose = new DatagramSocket();
                 udpIntrinsicsColor = new DatagramSocket();
                 udpIntrinsicsFisheye = new DatagramSocket();
+                udpWIFIScan = new DatagramSocket();
             } catch (SocketException e) {
                 e.printStackTrace();
             }
@@ -192,6 +234,7 @@ public class HelloTangoActivity extends Activity {
             pointCloudThread = new Thread(new Runnable() {
                 public void run() {
                     while (true) {
+                        int max_floats_per_packet = 15000;
                         float[] currPointCloud = TangoJNINative.returnPointCloud();
                         //imagesColorThread.interrupt();
                         System.out.println("POINT CLOUD SIZE " + currPointCloud.length);
@@ -200,18 +243,39 @@ public class HelloTangoActivity extends Activity {
                             try {
                                 udpPayload.write("POINTCLOUDSTARTINGRIGHTNOW\n".getBytes());
                                 // was throttling this before
-                                int currPointCloudLength = currPointCloud.length > 15000 ? 15000 : currPointCloud.length;
-                                byte buf[] = new byte[4 * currPointCloudLength];
+                                byte[] buf;
+                                int remainingFloats = currPointCloud.length;
 
-                                for (int i = 0; i < currPointCloudLength; ++i) {
+                                if (remainingFloats > max_floats_per_packet) {
+                                    buf = new byte[4*max_floats_per_packet];
+                                } else {
+                                    buf = new byte[4*remainingFloats];
+                                }
+                                int buffPosition = 0;
+
+                                for (int i = 0; i < currPointCloud.length; ++i) {
+                                    if (buffPosition >= max_floats_per_packet) {
+                                        udpPayload.write(buf);
+                                        byte[] payload = udpPayload.toByteArray();
+                                        udpPointCloud.send(new DatagramPacket(payload, payload.length, remote, portNumberPointCloud));
+                                        udpPayload = new ByteArrayOutputStream();
+                                        buffPosition = 0;
+                                        if (remainingFloats > max_floats_per_packet) {
+                                            buf = new byte[4*max_floats_per_packet];
+                                        } else {
+                                            System.out.println("POINT allocating partial packet " + remainingFloats);
+                                            buf = new byte[4*remainingFloats];
+                                        }
+                                    }
                                     int val = Float.floatToRawIntBits(currPointCloud[i]);
-                                    buf[4 * i] = (byte) (val >> 24);
-                                    buf[4 * i + 1] = (byte) (val >> 16);
-                                    buf[4 * i + 2] = (byte) (val >> 8);
-                                    buf[4 * i + 3] = (byte) (val);
+                                    buf[4 * buffPosition] = (byte) (val >> 24);
+                                    buf[4 * buffPosition + 1] = (byte) (val >> 16);
+                                    buf[4 * buffPosition + 2] = (byte) (val >> 8);
+                                    buf[4 * buffPosition + 3] = (byte) (val);
+                                    buffPosition++;
+                                    remainingFloats--;
                                 }
                                 udpPayload.write(buf);
-                                udpPayload.write("\n".getBytes());
                                 udpPayload.write("POINTCLOUDENDINGRIGHTNOW\n".getBytes());
                                 byte[] payload = udpPayload.toByteArray();
                                 udpPointCloud.send(new DatagramPacket(payload, payload.length, remote, portNumberPointCloud));
@@ -376,15 +440,44 @@ public class HelloTangoActivity extends Activity {
                     }
                 }
             });
+
+            scanThread = new Thread(new Runnable() {
+                public void run() {
+                    while (true) {
+                        if (connected) {
+                            if (scanPacket != null) {
+                                System.out.println("RSSI sending new packet\n");
+                                try {
+                                    udpWIFIScan.send(scanPacket);
+                                    scanPacket = null;
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                        try {
+                            // throttle the speed at which we send data
+                            // TODO: allow this to be configured via some sort of user interface widget
+                            Thread.sleep(100);
+                        } catch (InterruptedException ex) {
+                            System.err.println("Something weird happened");
+                        }
+                    }
+                }
+            });
             // start all of the threads
             // TODO: allow these to be turned on and off from the app
-            poseThread.start();
-            poseAreaThread.start();
-            pointCloudThread.start();
-            intrinsicsColorThread.start();
-            intrinsicsFisheyeThread.start();
-            imagesColorThread.start();
-            imagesFisheyeThread.start();
+            if (!scanWifi) {
+                poseThread.start();
+                poseAreaThread.start();
+                pointCloudThread.start();
+                intrinsicsColorThread.start();
+                intrinsicsFisheyeThread.start();
+                imagesColorThread.start();
+                imagesFisheyeThread.start();
+            } else {
+                scanThread.start();
+            }
         }
 
         public void onServiceDisconnected(ComponentName name) {
@@ -484,16 +577,24 @@ public class HelloTangoActivity extends Activity {
     protected void onResume() {
         super.onResume();
         TangoInitializationHelper.bindTangoService(this, mTangoServiceConnection);
+        if (scanWifi) {
+            IntentFilter rssiFilter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+            this.registerReceiver(myRssiChangeReceiver, rssiFilter);
+            System.out.println("RSSI: requesting scan");
+            WifiManager wifiMan= (WifiManager)getSystemService(Context.WIFI_SERVICE);
+            wifiMan.startScan();
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        if (scanWifi) {
+            unregisterReceiver(myRssiChangeReceiver);
+        }
         // TODO: this behavior doesn't work properly (App will always crash on pause)
         System.out.println("MYDEBUG: DISCONNECTING FROM TANGO!!!");
         TangoJNINative.disconnect();
-        synchronized (this) {
-            unbindService(mTangoServiceConnection);
-        }
+        unbindService(mTangoServiceConnection);
     }
 }
