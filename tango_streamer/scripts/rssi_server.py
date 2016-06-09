320a61586081c9762d80ca54973f694e2fb22190
Dhasharath Shrivathsa <Dhash@outlook.com>
2016-06-09 14:03:07 -0400

    Added timestamping through ROS topic

--- tango_streamer/scripts/rssi_server.py
+++ tango_streamer/scripts/rssi_server.py
@@ -2,22 +2,29 @@
 
 from udp import UDPhandle
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Float64
 
 
 rospy.init_node("RSSI")
 
 pub = rospy.Publisher("RSSI", String, queue_size=10)
+tango_time = rospy.Subscriber("/tango_clock", Float64, most_recent_ts)
 
 port = 11118
 
 start_delim = "RSSISTART\n"
 end_delim = "RSSIEND\n"
 
+ts = 0
+
+def most_recent_ts(msg):
+    ts = float(msg.data)
 
 
 @UDPhandle(port=port, start_delim=start_delim, end_delim=end_delim)
 def handle_pkt(pkt=None):
-    pub.publish(pkt)
+    global ts
+   
+    pub.publish(str(ts) + '\n' + pkt)
 
 handle_pkt()
