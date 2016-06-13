a819e02627d3624e891e0c8ca2b4bb06f2af8301
Dhasharath Shrivathsa <Dhash@outlook.com>
2016-06-13 11:02:23 -0400

    added custom message format for RSSI

--- tango_streamer/scripts/rssi_server.py
+++ tango_streamer/scripts/rssi_server.py
@@ -3,11 +3,11 @@
 from udp import UDPhandle
 import rospy
 from std_msgs.msg import String, Float64
-
+from tango_streamer.msg import Rf_stamped
 
 rospy.init_node("RSSI")
 
-pub = rospy.Publisher("RSSI", String, queue_size=10)
+pub = rospy.Publisher("RSSI", Rf_stamped, queue_size=10)
 tango_time = rospy.Subscriber("/tango_clock", Float64, most_recent_ts)
 
 port = 11118
@@ -18,13 +18,14 @@ end_delim = "RSSIEND\n"
 ts = 0
 
 def most_recent_ts(msg):
+    global ts    
     ts = float(msg.data)
 
 
 @UDPhandle(port=port, start_delim=start_delim, end_delim=end_delim)
 def handle_pkt(pkt=None):
     global ts
-   
-    pub.publish(str(ts) + '\n' + pkt)
+    out = Rf_stamped(header=Header(stamp=rospy.Time.now() + ts, frame_id="device"), data=str(pkt))
+    pub.publish(out)
 
 handle_pkt()
