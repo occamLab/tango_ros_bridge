5dbb761c6e834553859b11df6d7c3a32167188b8
William Derksen <william.derksen@students.olin.edu>
2017-06-20 10:02:27 -0400

    Moving from Point Cloud to Point Cloud 2

--- tango_streamer/scripts/pointcloud_server.py
+++ tango_streamer/scripts/pointcloud_server.py
@@ -5,8 +5,9 @@ import zlib
 import array
 import rospy
 import time
-from std_msgs.msg import Float64
-from sensor_msgs.msg import CompressedImage, PointCloud
+from std_msgs.msg import Float64, Header
+from sensor_msgs.msg import CompressedImage, PointCloud, PointCloud2, PointField
+from sensor_msgs.point_cloud2 import create_cloud_xyz32
 from geometry_msgs.msg import PoseStamped, Point32
 import sys
 
@@ -23,35 +24,46 @@ def handle_tango_clock(msg):
         tango_clock_offset = msg.data
 
 clock_sub = rospy.Subscriber('/tango_clock', Float64, handle_tango_clock)
-pub_point_cloud = rospy.Publisher('/point_cloud', PointCloud, queue_size=10)
+pub_point_cloud = rospy.Publisher('/point_cloud', PointCloud2, queue_size=10)
 
 rospy.init_node("pointcloud_stream")
 
-begin_point_cloud_marker = 'POINTCLOUDSTARTINGRIGHTNOW\n' 
+begin_point_cloud_marker = 'POINTCLOUDSTARTINGRIGHTNOW\n'
 end_point_cloud_marker = 'POINTCLOUDENDINGRIGHTNOW\n'
 
 
 @UDPhandle(port=11112, start_delim=begin_point_cloud_marker, end_delim=end_point_cloud_marker)
 def handle_pkt(pkt=None):
-    global pub_point_cloud 
+    global pub_point_cloud
     if len(pkt) % 4 != 0:
         print "WARNING! BAD POINTCLOUD PACKET"
         return
-    point_cloud_vals = array.array('f')
+    msg = PointCloud2()
+    point_cloud_vals = array.array('B')
     point_cloud_vals.fromstring(pkt[:4*(len(pkt)//4)])
-    point_cloud_vals.byteswap()
-    timestamp = point_cloud_vals[0]
-    point_cloud_vals = point_cloud_vals[1:]
+    #point_cloud_vals.byteswap()
+    #timestamp = #point_cloud_vals[0:4]
+    point_cloud_vals = point_cloud_vals[4:]
     if len(point_cloud_vals)>1:
-        point_cloud_vals = [float(p) for p in point_cloud_vals]
-        msg = PointCloud()
-        msg.header.stamp = rospy.Time(tango_clock_offset + float(timestamp))
-        msg.header.frame_id = 'depth_camera'
-        for i in range(0,len(point_cloud_vals)-3,4):
-            if abs(point_cloud_vals[i]) < 10**-5 or abs(point_cloud_vals[i+1]) < 10**-5 or abs(point_cloud_vals[i+2]) < 10**-5:
-                continue
+
+        msg.header = Header(stamp = rospy.Time.now(), frame_id='depth_camera')
+        msg.data = point_cloud_vals
+        msg.height = 1
+        msg.width = len(point_cloud_vals)/16
+        msg.point_step = 16
+        msg.is_dense = True
+        pointfields = []
+        names = ('x','y','z','c')
+        for i in range(4):
+            pointfields.append(PointField(name = names[i], offset = 4*i, datatype= 7, count = 0))
+        msg.fields = pointfields
+        #msg = create_cloud_xyz32(newheader, point_cloud_vals)
+        #for i in range(0,len(point_cloud_vals)-3,4):
+        #    if abs(point_cloud_vals[i]) < 10**-5 or abs(point_cloud_vals[i+1]) < 10**-5 or abs(point_cloud_vals[i+2]) < 10**-5:
+        #        continue
             # note we are currently ignoring the confidence value
-            msg.points.append(Point32(x=point_cloud_vals[i], y=point_cloud_vals[i+1], z=point_cloud_vals[i+2]))
+        #    msg.points.append(Point32(x=point_cloud_vals[i], y=point_cloud_vals[i+1], z=point_cloud_vals[i+2]))
+        print(msg.width)
         pub_point_cloud.publish(msg)
 
 handle_pkt()
